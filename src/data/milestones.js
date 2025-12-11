c3e2fd291f3b5481272dd0c3c6730cb6a10c4fcd
Gabriel Windlin <gawindlin@gmail.com>
2025-12-11 17:17:59 +0100

    anoter event

--- src/data/milestones.js
+++ src/data/milestones.js
@@ -5,9 +5,10 @@ export const relationshipStartDate = "2024-12-19";
 export const historicalMilestones = [
   { date: "2024-08-10", event: "O dia que nos conhecemos" },
   { date: "2024-12-19", event: "Pedido de namoro!" },
-  { date: "2025-04-11", event: "Primeira vinda sua a Suica!" },
+  { date: "2025-04-11", event: "Primeira viagem a Suica!" },
   {
     date: "2025-08-25",
     event: "Minha segunda vez indo ao Brasil para te ver!",
   },
+  { date: "2026-02-02", event: "Segunda viagem a Suica!" },
 ];
